234fab0101903c091a1f408eab65ed78ae1dfd8e
Teodor Pomohaci <97389197+VineBoomSoundEffect@users.noreply.github.com>
2022-07-26 14:19:42 +0300

    ReWrote the entire thing
    Probably made it worse, but I think it's for the better. Got rid of custom functions and other useless stuff. It's all in the power of scoping now.

--- corda.cpp
+++ corda.cpp
@@ -1,93 +1,77 @@
+//CORDA by Teodor Pomohaci, aka VineBoomSoundEffect on GitHub
+//This software is distributed under the MIT licence. Basically:
+// - you can do whatever you want with this
+// - even use it commercially if you want
+// - just don't say you own it
+// - if you do use it, I'd appreciate if you gave me credit :)
 #include <iostream>
 #include <fstream>
 #include <windows.h>
-#include <conio.h>
 #include <cstring>
 #include <string>
-
 using namespace std;
 
-char ch; //current pressed character
-int i = 0; //global incrementor
-string path; //path to %localappdata%
-ifstream fin; //^^^^^^^^^^^^^^^^^^^^^
-struct{
-    char ch[40];
-    char str[200];
-}f[200];
-
-//FUNCTION TO CHECK IF A KEY IS PRESSED
-//1 - key is pressed
-//0 - key is not pressed
-int KeyPress(char ch){
-    if(GetAsyncKeyState(ch) < 0) return 1;
-    else return 0;
-}
-
-//wanted to make this a function, but that destroys the sync(whatever that means)
-#define KeyToggle(x) GetKeyState(x)>0
-
-//FINALLY MANAGED AT LEAST SOMETHING WITH CHORDS
-//currently struggling with chords that have the same keys 
-void Input(char ch[40], char str[200]){
-    int keystate = 0;
-    for(int i=1;KeyPress(ch[i-1]) && i<=strlen(ch);i++){
-        if(i == strlen(ch) && GetAsyncKeyState(VkKeyScan('`')) || GetAsyncKeyState(VkKeyScan('~'))){
-            INPUT ip[strlen(str)];
-            for(int j=0;j<strlen(ch)+1;j++){
-            	ip[j].type = INPUT_KEYBOARD;
-            	ip[j].ki.dwFlags = 0;
-            	ip[j].ki.wVk = VK_BACK;
-            }
-            SendInput(strlen(ch)+1, ip, sizeof(INPUT));
-            for(int j=0;j<strlen(str);j++){
-            	ip[j].type = INPUT_KEYBOARD;
-            	ip[j].ki.dwFlags = KEYEVENTF_UNICODE;
-            	ip[j].ki.wVk = 0;
-                ip[j].ki.wScan = str[j];
-            }
-            SendInput(strlen(str), ip, sizeof(INPUT));
-            ip[0].ki.wScan = ' ';
-            SendInput(1, &ip[0], sizeof(INPUT));
-            keystate = 1;
-        }
-    }
-    while(keystate){
-        for(int i=1;!KeyPress(ch[i-1]) && i<=strlen(ch);i++){
-            if(i == strlen(ch)){
-                keystate = 0;
-            }
-        }
-    }
-}
+ifstream fin;
+string path; //path to localappdata
+struct eg{
+	char ch[40];
+	char str[300];
+}f[200]; //the chords and words will be stored here
+int k = 0; //global incrementor, useful for knowing how many chord-output pairs are there
 
 int main(int argc, char ** argv){
-    if(argc > 1){
-        cout << "The program has started.\n";
-        //messy std::string stuff to read from %localappdata%
-        path = getenv("localappdata");
-        path += "\\corda\\";
-        path += argv[1];
-        path += ".txt";
-        fin.open(path.c_str(), ios::out | ios::app);
-
-        for(/*Global Incrementor*/ i=0;i<10;i++){
-            fin.get(f[i].ch, 40);
-            fin.get();
-            fin.get(f[i].str, 200);
-            fin.get();
-        }
-        while(1){
-            for(int j=0;j<i;j++){
-                if(KeyPress(VK_TAB)) cout << "asdf\n";
-                Input(f[j].ch, f[j].str);
-            }
-            Sleep(10); //fixes performance issues, deleting this results in 100% cpu usage.
-			//i am aware that the more complex a txt file becomes, the less responsive the app will feel.
-			//but i can't do anything about it.
-        }
-    }
-    return 0;
+	cout << "The program has started.\n";
+	if(argc >= 1){
+		//messy read from localappdata/corda
+		path = getenv("localappdata");
+		path += "\\corda\\";
+		path += argv[1];
+		path += ".txt";
+		fin.open(path.c_str(), ios::out | ios::app);
+		//get the chords and respective outputs and store them
+		for(k=0;fin.get(f[k].ch, 40);k++){
+			fin.get();
+			fin.get(f[k].str, 300);
+			fin.get();
+		}
+		while(1){
+			//loop through each chord-output pair
+			for(int i=0;i<k && GetAsyncKeyState(VkKeyScan('`'));i++){
+				int keystate = 0;
+				for(int j=1;j<=strlen(f[i].ch) && GetAsyncKeyState(VkKeyScan(f[i].ch[j-1]));j++){
+					if(j == strlen(f[i].ch)){
+						INPUT ip[strlen(f[i].str)];
+						//delete the characters from the chord
+						for(int l=0;l<=strlen(f[i].ch);l++){
+							ip[l].type = INPUT_KEYBOARD;
+							ip[l].ki.dwFlags = KEYEVENTF_UNICODE;
+							ip[l].ki.wVk = 0;
+							ip[l].ki.wScan = 8;
+						}
+						//then, simulate the output
+						SendInput(strlen(f[i].ch)+1, ip, sizeof(INPUT));
+						for(int l=0;l<strlen(f[i].str);l++){
+							ip[l].type = INPUT_KEYBOARD;
+							ip[l].ki.dwFlags = KEYEVENTF_UNICODE;
+							ip[l].ki.wVk = 0;
+							ip[l].ki.wScan = f[i].str[l];
+						}
+						SendInput(strlen(f[i].str), ip, sizeof(INPUT));
+						//then add a space 
+						ip[0].ki.wScan = ' ';
+						SendInput(1, &ip[0], sizeof(INPUT));
+						keystate = 1;
+					}
+				}
+				//wait until all keys have been unpressed
+				while(keystate){
+					for(int j=1;j<=strlen(f[i].ch) && !GetAsyncKeyState(VkKeyScan(f[i].ch[j-1]));j++){
+						if(j == strlen(f[i].ch)) keystate = 0;
+					}
+				}
+			}
+			Sleep(10); //for better performance, otherwise the cpu is always at 100%
+		}
+	}
+	return 0;
 }
-//ISSUSE: same lettered chords dont work
-//fixed 100% cpu usage when idle, although it will still go up if a chord is pressed
